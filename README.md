bb4b25058038ccf2666df83094458155896a966e
Flo Kleiser <floriankleiser@gmail.com>
2025-11-20 09:59:56 +0100

    exercise1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Bits-and-Atoms-3 Flo
 
 This is the readme for the Bits & Atoms 3 repo (data visualisation)
+
+### Useful links:
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-const width = 640;
-const height = 400;
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
-
-const x = d3.scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-    .range([marginLeft, width - marginRight]);
-
-const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
-
-const svg = d3.create("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-svg.append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
-
-svg.append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y));
-
-container.append(svg.node());
-
+fetch('datasets/temp.json')
+    .then(response => response.json())
+    .then(data => {
+        document.body.innerHTML = `<p1>${JSON.stringify(data, null, 2)}</p1>`;
+    })
+    // .then(data => { console.log(data) })
+    .catch(error => {
+        console.error('Error:', error);
+    });
